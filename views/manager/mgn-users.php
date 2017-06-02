ac6e0e68c6b1984d5691a8998ef9c8cf2feb7a6c
thrawniejoe <thrawniejoe@gmail.com>
2017-06-02 17:46:34 -0400

    added error page and mgn-users file

---
+++ views/manager/mgn-users.php
@@ -0,0 +1,56 @@
+<?php include '../views/shared/header_backend.php'; ?>
+<?php include '../views/shared/sidebar.php'; ?>
+<main>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Dashboard - <?php echo $_SESSION['currentUser']->getUsername();?></h1>
+          
+        
+  <div class="container">
+    <div class="panel panel-default buffer">
+      <div class="panel-body">
+        <form action="../controllers/managerController.php" method="post">
+                <input type="hidden" name="action" value="add_project">
+                <input class="btn btn-success" type="submit" value="Add Project">
+        </form>
+        <h2>Select Project</h2>
+        <table class="table">
+          <tr class="panel-heading">
+            <th>Name</th>
+            <th>Description</th>
+            <th>Image Link</th>
+            <th>&nbsp;</th>
+          </tr>
+          <?php foreach ($users as $user) : ?>
+          <tr>
+            <td>
+              <?php echo htmlspecialchars($user['projectName']); ?>
+            </td>
+            <td>
+              <?php echo htmlspecialchars($user['description']); ?>
+            </td>
+            <td>
+              <?php echo htmlspecialchars($user['sampleSite_Link']); ?>
+            </td>
+            <td>
+              <form action="../controllers/managerController.php" method="post">
+                <input type='hidden' name='project_code' value=<?php echo htmlspecialchars($user['id']); ?>>
+                <input type="hidden" name="action" value="edit_project">
+                <input class="btn btn-default" type="submit" value="modify">
+              </form>
+            </td>
+            <td>
+              <form action="../controllers/managerController.php" method="post">
+                <input type='hidden' name='project_code' value=<?php echo htmlspecialchars($user['id']); ?>>
+                <input type="hidden" name="action" value="delete_project">
+                <input class="btn btn-default" type="submit" value="delete">
+              </form>
+            </td>
+          </tr>
+          <?php endforeach; ?>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+</main>
+<?php include '../views/shared/footer.php'; ?>
